f25137334de97b3b8e876cda11045dd7f10149ec
Mathias <majohha@gmail.com>
2020-04-28 16:03:37 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get("/mushrooms", () => {
+    //...
+})
+
+app.get("/mushroom/:id", () => {
+    //...
+})
+
+app.post("/mushroom/:id", () => {
+    //...
+})
---
+++ init-db.js
@@ -0,0 +1,3 @@
+var MongoClient = require('mongodb').MongoClient
+var 
+var url = `mongodb://${host}`
